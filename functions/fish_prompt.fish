2e8da497a8a2ee5f8a49250ca2135e189e1b1697
Alexey <symblight@gmail.com>
2022-05-06 11:07:35 +0300

    updates

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -4,15 +4,15 @@
 function __user_host
   set -l content 
   if [ (id -u) = "0" ];
-    echo -n (set_color --bold red)
+    echo -n (set_color --bold blue)
   else
-    echo -n (set_color --bold green)
+    echo -n (set_color --bold yellow)
   end
-  echo -n $USER@(hostname|cut -d . -f 1) (set color normal)
+  echo -n $USER(set_color white)@(set_color --bold blue)(hostname|cut -d . -f 1) (set color normal)
 end
 
 function __current_path
-  echo -n (set_color --bold blue) (pwd) (set_color normal) 
+  echo -n (set_color white)':'(set_color --bold magenta)(pwd | awk -F/ '{print $NF}')(set_color normal) 
 end
 
 function _git_branch_name
@@ -28,35 +28,22 @@ function __git_status
     set -l git_branch (_git_branch_name)
 
     if [ (_git_is_dirty) ]
-      set git_info '<'$git_branch"*"'>'
+      set git_info (set_color white)'('(set_color red)$git_branch"*"(set_color white)')'
     else
-      set git_info '<'$git_branch'>'
+      set git_info (set_color white)'('(set_color red)$git_branch(set_color white)')'
     end
 
-    echo -n (set_color yellow) $git_info (set_color normal) 
+    echo -n (set_color --bold red)$git_info (set_color normal) 
   end
-end
-
-function __ruby_version
-  if type "rvm-prompt" > /dev/null 2>&1
-    set ruby_version (rvm-prompt i v g)
-  else if type "rbenv" > /dev/null 2>&1
-    set ruby_version (rbenv version-name)
-  else
-    set ruby_version "system"
-  end
-
-  echo -n (set_color red) ‹$ruby_version› (set_color normal)
 end
 
 function fish_prompt
-  echo -n (set_color white)"╔ "(set_color normal)
+  echo -n (set_color --bold white)"╔ "(set_color normal)
   __user_host
   __current_path
-  __ruby_version
   __git_status
   echo -e ''
-  echo (set_color white)"╚ "(set_color --bold white)"\$ "(set_color normal)
+  echo (set_color --bold white)"╚ "(set_color --bold blue)"\$ "(set_color normal)
 end
 
 function fish_right_prompt
